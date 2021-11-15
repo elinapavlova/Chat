23eac36957b32306a223238f96678f0116e60383
elinapavlova <elina.pavlova.02@mail.ru>
2021-11-15 09:27:11 +0300

    Delete unused namespaces in IFileStorageService

--- Services/IFileStorageService.cs
+++ Services/IFileStorageService.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Infrastructure.Result;
-using Models;
 using Models.FileModel;
 using Models.UploadModel;
 
